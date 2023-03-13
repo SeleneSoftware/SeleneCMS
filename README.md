e7fd7c5931b390530a376a1a3cfd6df76cb462b6
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-03-13 11:45:12 -0400

    found a base template I like, mostly

---
+++ README.md
@@ -0,0 +1,19 @@
+# FedImg
+
+FedImg is a Federated Image Sharing Social Network.  Similar to Instagram
+
+## Installation
+
+Clone the repository and run the installer.
+
+```bash
+php bin/console install:new
+```
+
+## Contributing
+
+Please visit CONTRIBUTING file for more information
+
+## License
+
+[GPLv3](https://choosealicense.com/licenses/gpl-3.0/)
--- assets/app.js
+++ assets/app.js
@@ -5,16 +5,17 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import "./js/jquery.dropotron.min.js";
+import "./js/browser.min.js";
+import "./js/breakpoints.min.js";
+import "./js/util.js";
+import "./js/main.js";
+
+// require('font-awesome/css/font-awesome.css');
+
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+// SCSS files can be compiled here the same way
+import "./css/main.css";
 
 // start the Stimulus application
-import './bootstrap';
-
-// import 'jquery.scrollex';
-
-import './js/jquery.scrolly.js';
-var skel = require('./js/skel.js');
-// import './js/skel.js';
-import './js/util.js';
-import './js/main.js';
+import "./bootstrap";
