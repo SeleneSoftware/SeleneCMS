ed91cc53bc3e636367e5159eb1768bdd549cbe24
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-06-11 12:52:50 -0400

    fixing an api change

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 use Selene\CMSBundle\Controller\BlogController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends BlogController
 {
     #[Route('/', name: 'app_default')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(EntityManagerInterface $doctrine): Response
     {
         return $this->render('default/index.html.twig', [
             'recentBlogs' => $this->getBlogList($doctrine, 3),
