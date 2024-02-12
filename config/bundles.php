d8a99385aea1bbe8e23c91164ebc28d8d222ec01
Jason Marshall <jasonmarshallselene@gmail.com>
2024-02-12 13:32:22 -0500

    working on the new version for the cms-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
