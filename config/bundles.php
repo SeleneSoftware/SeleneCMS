4b7e74e66c9ea588c195e3767811b3e2622353ca
Jason Marshall <jasonmarshallselene@gmail.com>
2024-03-20 17:26:50 -0400

    the footer works as a component

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
