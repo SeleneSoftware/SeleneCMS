b7dcb58fc59e2f7dadb6f7cbb15b775477fa5ef2
Jason Marshall <jasonmarshallselene@gmail.com>
2024-09-09 11:37:10 -0400

    new sample theme for skeleton, everybody welcome Editorial

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,10 @@
+import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
  */
+import './styles/app.css';
 
-import "./js/jquery.dropotron.min.js";
-import "./js/browser.min.js";
-// import "./js/breakpoints.min.js";
-import "breakpoints-js";
-import "./js/util.js";
-import "./js/main.js";
-
-// require('font-awesome/css/font-awesome.css');
-
-// any CSS you import will output into a single css file (app.css in this case)
-// SCSS files can be compiled here the same way
-import "./css/main.css";
-
-// start the Stimulus application
-import "./bootstrap";
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
