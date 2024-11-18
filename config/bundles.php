b4305293ac29e27ba1e0dfc2afbdab56dcfdff87
Jason Marshall <jasonmarshallselene@gmail.com>
2024-11-17 19:19:48 -0500

    more fixes for the  new version 0.1.0

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Selene\CMSBundle\seleneCMSBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
